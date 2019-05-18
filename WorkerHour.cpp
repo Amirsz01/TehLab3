d68f0305f8d02c816e4cab64be596786bb95fd2c
Amirsz01 <43276827+Amirsz01@users.noreply.github.com>
2019-05-18 17:18:32 +0300

    Update WorkerHour.cpp

--- WorkerHour.cpp
+++ WorkerHour.cpp
@@ -8,33 +8,57 @@ int WorkerHour::cash()
 WorkerHour::~WorkerHour()
 {
 	ofstream out("WorkerHour.txt");
-	out << name << " " << fname << " " << phone << " " << adress << " " << hours << " " << traf;
+	out << getData(1) << " " << getData(2) << " " << getData(3) << " " << getData(4) << " " << hours << " " << traf;
+	out.close();
 }
 
 WorkerHour::WorkerHour()
 {
 	ifstream in("WorkerHour.txt");
-	if (!in || is_empty(in))
+	string callback;
+	int flag;
+	cout << "Ввести вручную или загрузить из файла? 1/0" << endl;
+	cin >> flag;
+	if (!flag)
 	{
-		cout << "Файл Пуст" << endl;
-		system("pause");
+		if (!in || is_empty(in)) 
+		{
+			cout << "Файл Пуст" << endl;
+			system("pause");
+			flag = 1;
+		}
+	}
+	if (flag)
+	{
 		system("cls");
 		cout << "Введите Имя" << endl;
-		cin >> this->name;
+		cin >> callback;
+		setData(1, callback);
 		cout << "Введите Фамилию" << endl;
-		cin >> this->fname;
+		cin >> callback;
+		setData(2, callback);
 		cout << "Введите Телефон" << endl;
-		cin >> this->phone;
+		cin >> callback;
+		setData(3, callback);
 		cout << "Введите Адрес" << endl;
-		cin >> this->adress;
+		cin >> callback;
+		setData(4, callback);
 		cout << "Введите количество часов" << endl;
 		cin >> hours;
 		cout << "Введите цену за час работы" << endl;
 		cin >> traf;
 	}
-	else
+	else 
 	{
-		in >> name >> fname >> phone >> adress >> hours >> traf;
+		in >> callback;
+		setData(1, callback);
+		in >> callback;
+		setData(2, callback);
+		in >> callback;
+		setData(3, callback);
+		in >> callback;
+		setData(4, callback);
+		in >> hours >> traf;
 	}
 	in.close();
 }
