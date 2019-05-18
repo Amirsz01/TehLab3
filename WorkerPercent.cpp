97e47519e2e76bff62a099cbb6d1cc93c59061fc
Amirsz01 <43276827+Amirsz01@users.noreply.github.com>
2019-05-18 17:18:45 +0300

    Update WorkerPercent.cpp

--- WorkerPercent.cpp
+++ WorkerPercent.cpp
@@ -3,34 +3,59 @@
 WorkerPercent::WorkerPercent() 
 {
 	ifstream in("WorkerPecent.txt");
-	if (!in || is_empty(in))
+	string callback;
+	int flag;
+	cout << "Ввести вручную или загрузить из файла? 1/0" << endl;
+	cin >> flag;
+	if (!flag)
 	{
-		cout << "Файл Пуст" << endl;
-		system("pause");
+		if (!in || is_empty(in))
+		{
+			cout << "Файл Пуст" << endl;
+			system("pause");
+			flag = 1;
+		}
+	}
+	if (flag)
+	{
 		system("cls");
 		cout << "Введите Имя" << endl;
-		cin >> this->name;
+		cin >> callback;
+		setData(1, callback);
 		cout << "Введите Фамилию" << endl;
-		cin >> this->fname;
+		cin >> callback;
+		setData(2, callback);
 		cout << "Введите Телефон" << endl;
-		cin >> this->phone;
+		cin >> callback;
+		setData(3, callback);
 		cout << "Введите Адрес" << endl;
-		cin >> this->adress;
-		cout << "Введите продажи за месяц" << endl;
+		cin >> callback;
+		setData(4, callback);
+		cout << "Введите продажы за месяц" << endl;
 		cin >> mounthSells;
 		cout << "Введите процент с продаж" << endl;
 		cin >> percent;
 	}
 	else
 	{
-		in >> name >> fname >> phone >> adress >> mounthSells >> percent;
+		in >> callback;
+		setData(1, callback);
+		in >> callback;
+		setData(2, callback);
+		in >> callback;
+		setData(3, callback);
+		in >> callback;
+		setData(4, callback);
+		in >> mounthSells >> percent;
 	}
+	in.close();
 }
 
 WorkerPercent::~WorkerPercent() 
 {
 	ofstream out("WorkerPercent.txt");
-	out << name << " " << fname << " " << phone << " " << adress << " " << mounthSells << " " << percent;
+	out << getData(1) << " " << getData(2) << " " << getData(3) << " " << getData(4) << " " << mounthSells << " " << percent;
+	out.close();
 }
 
 int WorkerPercent::cash() 
