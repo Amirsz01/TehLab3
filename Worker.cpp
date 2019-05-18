c3785c4911802e137597cad1cf6cef02bc73eb7a
Amirsz01 <43276827+Amirsz01@users.noreply.github.com>
2019-05-18 17:17:15 +0300

    Update Worker.cpp

--- Worker.cpp
+++ Worker.cpp
@@ -5,6 +5,46 @@ Worker::Worker()
 
 }
 
+string Worker::getData(int data)
+{
+	switch (data)
+	{
+	case 1:
+		return name;
+		break;
+	case 2:
+		return fname;
+		break;
+	case 3:
+		return phone;
+		break;
+	case 4:
+		return adress;
+		break;
+	}
+}
+
+void Worker::setData(int data, string sData)
+{
+	switch (data)
+	{
+	case 1:
+		name = sData;
+		break;
+	case 2:
+		fname = sData;
+		break;
+	case 3:
+		phone = sData;
+		break;
+	case 4:
+		adress = sData;
+		break;
+	default:
+		break;
+	}
+}
+
 Worker::~Worker()
 {
 
