f0f49733e373aa9a79c87065a7cb7f964d809715
Amirsz01 <43276827+Amirsz01@users.noreply.github.com>
2019-05-18 17:16:33 +0300

    Update Worker.h

--- Worker.h
+++ Worker.h
@@ -1,19 +1,61 @@
-#pragma once
-#include <iostream>
-#include <fstream>
-#include <string>
-using namespace std;
-class Worker
+#include </Users/Amirsz/source/repos/ТестовыйСтенд/ТестовыйСтенд3/Worker.h>
+
+Worker::Worker()
 {
-	string name;
-	string fname;
-	string phone;
-	string adress;
-public:
-	Worker();
-	string getData(int data);
-	void setData(int data, string sData);
-	virtual ~Worker();
-	virtual int cash();
-	bool is_empty(ifstream& pFile);
-};
+
+}
+
+string Worker::getData(int data)
+{
+	switch (data)
+	{
+	case 1:
+		return name;
+		break;
+	case 2:
+		return fname;
+		break;
+	case 3:
+		return phone;
+		break;
+	case 4:
+		return adress;
+		break;
+	}
+}
+
+void Worker::setData(int data, string sData)
+{
+	switch (data)
+	{
+	case 1:
+		name = sData;
+		break;
+	case 2:
+		fname = sData;
+		break;
+	case 3:
+		phone = sData;
+		break;
+	case 4:
+		adress = sData;
+		break;
+	default:
+		break;
+	}
+}
+
+Worker::~Worker()
+{
+
+}
+
+int Worker::cash()
+{
+	return 0;
+}
+
+bool Worker::is_empty(ifstream& pFile)
+{
+	return pFile.peek() == -1;
+}
